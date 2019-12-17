cd2f1e97959c66c6b40237fa70f209c5adb112cf
Danny Hollman <dannyhollman@gmail.com>
2019-12-17 23:12:20 +0000

    [UPDATE] Remove unneeded nullable check

--- models/city.py
+++ models/city.py
@@ -14,4 +14,4 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     state_id = Column(String(60), ForeignKey(State.id), nullable=False)
     name = Column(String(128), nullable=False)
-    places = relationship("Place", backref="cities", nullable=False)
+    places = relationship("Place", backref="cities")
