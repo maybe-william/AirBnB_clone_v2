2a6a83f30acea20bb00c59ffe80d52d98314d781
Danny Hollman <dannyhollman@gmail.com>
2019-12-17 14:44:25 +0000

    [UPDATE] Add comments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,5 @@
+-- create hbnb_dev_db database
+-- create hbnb_dev user with permissions
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
 SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
