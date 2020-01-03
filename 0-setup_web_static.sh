259f9a64f0f44f8ff3f5f409699e9228d4a36ffc
will <818@holbertonschool.com>
2020-01-03 20:03:32 +0000

    [BUGFIX] Use service to start service

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,7 @@
 # install nginx if not installed
 sudo apt-get update
 sudo apt-get install -y nginx
-nginx -s start
+sudo service nginx start
 
 # create all directories and basic files if nonexistant
 sudo mkdir -p /data/web_static/releases/test/
@@ -23,5 +23,5 @@ sudo chown -R ubuntu:ubuntu /data
 echo $'server {\n\tadd_header X-Served-By '"$HOSTNAME"$';\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\troot /var/www/html;\n\tindex index.html index.htm index.nginx-debian.html;\n\tserver_name _;\n\terror_page 404 /404.html;\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n\tlocation /404.html {\n\t\troot /var/www/err/html;\n\t\tinternal;\n\t}\n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}\n\tlocation / {\n\t\ttry_files $uri $uri/ =404;\n\t}\n}\n' | sudo tee /etc/nginx/sites-available/default
 
 # reload and restart nginx
-nginx -s reload
-nginx -s restart
+sudo service nginx reload
+sudo service nginx restart
