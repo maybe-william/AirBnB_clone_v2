991760791d0734eb5b8b925e9a57655c95852ecd
will <818@holbertonschool.com>
2020-01-21 21:58:06 +0000

    in

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This module runs a flask application server"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_flask():
+    """root route returning the desired string"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """hbnb route returning the desired string"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_c(text):
+    """display C and then text"""
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def hello_python(text):
+    """display Python and then text"""
+    return "Python " + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
